f99e3d91e6641d5e8af1f6399da3a21cb81aa11b
Prudnikova <dakoto4ka@yandex.ru>
2022-10-31 11:49:14 +0300

    ДЗ 1 урока 3 выполнено.правки внесены

--- src/humans/Human.java
+++ src/humans/Human.java
@@ -11,12 +11,40 @@ public class Human {
         this.setYearOfBirth(yearOfBirth);
         this.setTown(town);
 
+
         if (name == null) {
             this.name = "Информация не укзана";
         } else {
             this.name = name;
         }
+        if (job == null) {
+            this.job = "Информация не укзана";
+        } else {
+            this.job = job;
+        }
+    }
 
+
+        public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        if(yearOfBirth<0){
+            this.yearOfBirth = 0;
+        }
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
         if (job == null) {
             this.job = "Информация не укзана";
         } else {
@@ -25,28 +53,12 @@ public class Human {
 
     }
 
-    public int getYearOfBirth() {
-        return yearOfBirth;
-    }
-
-    public String getTown() {
-        return town;
-    }
-
     public void setTown(String town) {
         if (town == null) {
             this.town = "Информация не указана";
         } else {
             this.town = town;
         }
-        public void setYearOfBirth(int yearOfBirth){
-            if (yearOfBirth < 0) {
-                this.yearOfBirth = 0;
-            }
-            this.yearOfBirth = yearOfBirth;
-
-        }
-
 
     }
 }
